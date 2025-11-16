3dae6901eb5c768494ed5a557c4ea8acd60e4f61
VidMahoric <vid.mahoric@student.um.si>
2025-11-16 22:39:27 +0100

    Izbrisane vse mape in datoteke

--- BookBuddy/Models/Komentar.cs
+++
@@ -1,8 +0,0 @@
-namespace BookBuddy.Models;
-
-public class Komentar
-{
-    public string Uporabnik { get; set; } = string.Empty;
-    public string Besedilo { get; set; } = string.Empty;
-    public DateTime Datum { get; set; }
-}
